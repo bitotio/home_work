13d6af735dab0d63f6b22b4f7c244c3ebda2e0a9
bitotio <glebushkapavl2009q@gmail.com>
2024-11-10 15:36:29 +0300

    add

--- 3/main.py
+++ 3/main.py
@@ -1,7 +1,5 @@
-# создание бота
-
 from tank import Tank
-from tkinter import*
+from tkinter import *
 
 
 
@@ -10,18 +8,17 @@ KEY_S = 83
 KEY_A = 65
 KEY_D = 68
 
-
 FPS = 60
+
 def update():
     player.update()
-# 5 вызовем движение бота так же как и танка
-    enemy.update()
     check_collision()
     w.after(1000//FPS, update)
 
 def check_collision():
-    player.inersects(enemy)
-    enemy.inersects(player)
+        if player.intersects(enemy):
+            print('Танки столкнулись')
+            player.undo_move()
 
 def key_press(event):
     if event.keycode == KEY_W:
@@ -32,20 +29,21 @@ def key_press(event):
         player.left()
     if event.keycode == KEY_D:
         player.right()
+    check_collision()
+
 
 
 w = Tk()
 w.title('Танки на минималках 2.0')
 canv = Canvas(w, width = 800, height = 600, bg = 'alice blue')
 canv.pack()
+player = Tank(canvas = canv, x = 100, y = 50, ammo = 100)
 
-player = Tank(canvas = canv, x = 100, y = 50, ammo = 100, speed=1, bot = False)
-enemy = Tank(canvas = canv, x = 300, y = 300, ammo = 100, speed=1, bot = True)
+enemy = Tank(canvas = canv, x = 300, y = 300, ammo = 100)
+
 
 
 w.bind('<KeyPress>', key_press)
-
-enemy.set_target(player)
 
 update()
 w.mainloop()
